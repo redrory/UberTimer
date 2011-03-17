94a52531d2b7f6e079455bc881052d5c75913dd3
Rory Walker <redrory@gmail.com>
2011-03-16 20:21:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ubertimer::Application.config.secret_token = 'eafcb7921f2fa5013aab1259eb348cc9db7b84dca9aff97b8d82afb3e4403e5d09bbedb14ca6a0dbb2ece9b1c0e76d452d89f814f555008dcafb4def39298268'
