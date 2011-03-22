f8650d92ff7b930979e45620814d2b504f8d23a9
Rory Walker <redrory@gmail.com>
2011-03-21 21:10:27 -0500

    Handling timer page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,25 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+	console.log('eee');
+	var seconds = 1;
+	var count = 1;
+	
+	/*setInterval( 
+			function(){
+	          document.getElementById('seconds').innerHTML = --seconds;
+			//	}, 10);
+	});
+	*/
+	
+	$("a.add").click(function(){	
+		$('<br>'+'<label for="text">I only have</label> '+
+			'<input id="time" name="time" size="9" type="text" value="30 minutes">' +
+			'<label for="projText">to focus on</label>' +
+			 '<input id="project" name="project" size="50" type="text" value="Web Development on Startup 2"/>').appendTo('ul');
+			count++;
+	//	$('<li>'+ "List "+'Mic Test' + '</li>').appendTo('ul');
+		console.log('In but'+count);		
+	
+		});
+console.log('eee2');
+});
+
